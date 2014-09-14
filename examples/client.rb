3c6f1477123a5ef4d1ea9434cfe0b83fa18ac9c9
Hendrik Beskow <hendrik@beskow.de>
2014-09-14 22:32:07 +0200

    added examples

---
+++ examples/client.rb
@@ -0,0 +1,9 @@
+﻿require 'bundler/setup'
+require 'ffi-czmq'
+
+client = CZMQ::Zsock.new_dealer('tcp://127.0.0.1:7000')
+
+4.times do
+  client.tell(nil, 'hello')
+  puts client.recv.to_a.inspect
+end
---
+++ examples/multi_threaded_server.rb
@@ -0,0 +1,73 @@
+﻿require 'bundler/setup'
+require 'ffi-czmq'
+
+class Worker
+  extend Forwardable
+
+  def_delegators :@parent_pipe, :<<, :tell, :recv, :wait, :destructor
+
+  def initialize(id)
+    @id = id
+    @parent_pipe = CZMQ::Zactor.new_actor(&method(:run))
+  end
+
+  private
+
+  def run(child_pipe)
+    @worker = CZMQ::Zsock.new_dealer('inproc://backend')
+
+    @reactor = CZMQ::Zloop.new
+    @reactor.set_verbose(true)
+    @reactor.add_reader(child_pipe, &method(:handle_pipe))
+    @reactor.add_reader(@worker, &method(:handle_worker))
+
+    child_pipe.signal(0)
+
+    @reactor.start
+  end
+
+  def handle_pipe(zsock)
+    msg = zsock.recv
+
+    case msg.first.to_str
+    when '$TERM'
+      -1
+    end
+  end
+
+  def handle_worker(zsock)
+    msg = zsock.recv
+
+    sender = msg.first
+    answer = CZMQ::Zmsg.new
+    answer << sender << nil << @id << 'welcome'
+    zsock.forward(answer)
+  end
+end
+
+proxy = CZMQ::Zactor.new_zproxy
+proxy << 'VERBOSE'
+
+proxy.tell('BACKEND', 'DEALER', 'inproc://backend')
+proxy.wait
+proxy.tell('FRONTEND', 'ROUTER', 'tcp://0.0.0.0:7000')
+proxy.wait
+
+workers = []
+4.times {|i| workers << Worker.new("worker#{i}") }
+
+trap('INT') do
+  workers.each do |worker|
+    worker.destructor
+  end
+  exit
+end
+
+trap('TERM') do
+  workers.each do |worker|
+    worker.destructor
+  end
+  exit
+end
+
+sleep
