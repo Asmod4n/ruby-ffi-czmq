35da08dbcb286c18eb1115feceb0a1d371c4c260
Asmod4n <Asmod4n@users.noreply.github.com>
2014-09-18 13:08:03 +0200

    Update client.rb

--- examples/client.rb
+++ examples/client.rb
@@ -2,7 +2,9 @@
 require 'ffi-czmq'
 
 config = CZMQ::Zconfig.load("#{File.dirname(__FILE__)}/examples.cfg")
-client = CZMQ::Zsock.new_dealer(config.resolve('/client/endpoint', nil))
+connect = config.resolve('/client/connect', nil)
+client = CZMQ::Zsock.new(config.resolve('/client/type', nil), connect)
+client.attach(connect, false)
 
 4.times do
   client.tell(nil, 'hello')
