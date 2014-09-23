0ebda9522e16260612d02f27b6ec82882b8db7eb
Asmod4n <Asmod4n@users.noreply.github.com>
2014-09-23 11:41:06 +0200

    Update zsys.rb

--- lib/czmq/zsys.rb
+++ lib/czmq/zsys.rb
@@ -10,14 +10,14 @@ module CZMQ
     czmq_function :info,          :info,          [:string],  :void
     czmq_function :debug,         :debug,         [:string],  :void
 
-    attach_variable :zsys_interrupted, :zsys_interrupted, :int
+    attach_variable :interrupted, :zsys_interrupted, :int
     attach_variable :zctx_interrupted, :zctx_interrupted, :int
 
     ENV['ZSYS_IPV6'] = '1'
     ENV['ZSYS_SIGHANDLER'] = 'false'
 
     at_exit do
-      zctx_interrupted = zsys_interrupted = 1
+      zctx_interrupted = interrupted = 1
     end
   end
 end
