a5bbf32d9341a89122a385258699172dcff03f9e
Hendrik Beskow <hendrik-github2@beskow.de>
2014-09-25 21:08:38 +0200

    fix zpoller func signature

--- lib/czmq/zpoller.rb
+++ lib/czmq/zpoller.rb
@@ -11,7 +11,7 @@ module CZMQ
     czmq_function :remove,      :remove,      [:pointer,  :pointer],  :int
     czmq_function :wait,        :wait,        [:pointer,  :int],      :pointer
     czmq_function :expired,     :expired,     [:pointer],             :bool
-    czmq_function :terminated,  :terminated,  [:pointer],             :pointer
+    czmq_function :terminated,  :terminated,  [:pointer],             :bool
 
     def self.new_poller(*pollitems)
       zsocks = []
