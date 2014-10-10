ce344fc76c2ce405ec69006cae079238759872ec
Hendrik Beskow <hendrik-github2@beskow.de>
2014-10-10 09:59:35 +0200

    add zstr again

--- lib/czmq/utils.rb
+++ lib/czmq/utils.rb
@@ -12,44 +12,44 @@ module CZMQ
     attach_function :errno,         :zmq_errno,       [],                                   :int,     blocking: true
     attach_function :strerror,      :zmq_strerror,    [:int],                               :string,  blocking: true
 
-    class << self
-      def version
-        unless @version
-          z_major = FFI::MemoryPointer.new :int
-          z_minor = FFI::MemoryPointer.new :int
-          z_patch = FFI::MemoryPointer.new :int
+    module_function
 
-          c_major = FFI::MemoryPointer.new :int
-          c_minor = FFI::MemoryPointer.new :int
-          c_patch = FFI::MemoryPointer.new :int
+    def version
+      unless @version
+        z_major = FFI::MemoryPointer.new :int
+        z_minor = FFI::MemoryPointer.new :int
+        z_patch = FFI::MemoryPointer.new :int
 
-          zmq_version  z_major, z_minor, z_patch
-          zsys_version c_major, c_minor, c_patch
+        c_major = FFI::MemoryPointer.new :int
+        c_minor = FFI::MemoryPointer.new :int
+        c_patch = FFI::MemoryPointer.new :int
 
-          @version = {
-             zmq: {major: z_major.read_int, minor: z_minor.read_int, patch: z_patch.read_int},
-            czmq: {major: c_major.read_int, minor: c_minor.read_int, patch: c_patch.read_int}
-          }
-        end
+        zmq_version  z_major, z_minor, z_patch
+        zsys_version c_major, c_minor, c_patch
 
-        @version
+        @version = {
+           zmq: {major: z_major.read_int, minor: z_minor.read_int, patch: z_patch.read_int},
+          czmq: {major: c_major.read_int, minor: c_minor.read_int, patch: c_patch.read_int}
+        }
       end
 
-      def check_for_pointer(ptr)
-        ptr.is_a?(FFI::Pointer) && !ptr.null?
-      end
+      @version
+    end
 
-      def error
-        strerror(errno)
-      end
+    def check_for_pointer(ptr)
+      ptr.is_a?(FFI::Pointer) && !ptr.null?
+    end
 
-      def bin2hex(bytes)
-        bytes.to_str.unpack(HEXY).first
-      end
+    def error
+      strerror(errno)
+    end
 
-      def hex2bin(hex)
-        [hex].pack(HEXY)
-      end
+    def bin2hex(bytes)
+      bytes.to_str.unpack(HEXY).first
+    end
+
+    def hex2bin(hex)
+      [hex].pack(HEXY)
     end
 
     if version[:czmq][:major] < 3
---
+++ lib/czmq/zstr.rb
@@ -0,0 +1,66 @@
+﻿require_relative 'libczmq'
+require_relative 'utils'
+require_relative 'zsock'
+
+module CZMQ
+  class Zstr
+    extend ::LibCZMQ
+
+    czmq_function :recv_zstr,  :recv,  [:pointer],                        :pointer
+    czmq_function :send_zstr,  :send,  [:pointer, :string],               :int
+    czmq_function :sendm,      :sendm, [:pointer, :string],               :int
+    czmq_function :sendx_zstr, :sendx, [:pointer, :string, :varargs],     :int
+    czmq_function :recvx_zsrt, :recvx, [:pointer, :buffer_out, :varargs], :int
+    czmq_function :free,       :free,  [:pointer],                        :void
+
+    class << self
+      def read_string(str_ptr)
+        if Utils.check_for_pointer(str_ptr)
+          str = str_ptr.read_string
+          FFI::MemoryPointer.new(:pointer) do |free_ptr|
+            free_ptr.write_pointer(str_ptr)
+            free(free_ptr)
+          end
+          str
+        else
+          fail ArgumentError, "#{str_ptr.class} is not a FFI::Pointer", caller
+        end
+      end
+
+      def recv(socket)
+        read_string(recv_zstr(Zsock.convert(socket)))
+      end
+
+      def sendx(socket, string, *strings)
+        if strings.size == 0
+          fail ArgumentError, "Wrong number of Arguments, (2 for 3..n)", caller
+        end
+        strings_varargs = ([ :string ] * strings.size).zip(strings).flatten
+        sendx_zstr(Zsock.convert(socket), string, *strings_varargs, :pointer, nil)
+      end
+
+      def recvx(socket, counter = 1)
+        if counter < 1
+          fail ArgumentError, "Counter must be >= 1, got #{counter}", caller
+        end
+        first_buffer = FFI::Buffer.new_out(:pointer, 1)
+        buffers = []
+        counter.times do
+          buffers << FFI::Buffer.new_out(:pointer, 1)
+        end
+        buffers_varargs = ([ :buffer_out ] * buffers.size).zip(buffers).flatten
+
+        recvx_zsrt(Zsock.convert(socket), first_buffer, *buffers_varargs, :pointer, nil)
+
+        strings = []
+        strings << first_buffer.read_pointer.read_string
+        free(first_buffer)
+        buffers.each do |buffer|
+          strings << buffer.read_pointer.read_string
+          free(buffer)
+        end
+        strings
+      end
+    end
+  end
+end
