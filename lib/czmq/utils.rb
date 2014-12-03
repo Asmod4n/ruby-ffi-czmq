b0cf053d087c0999248c800e5510515f67e5e542
Asmod4n <Asmod4n@users.noreply.github.com>
2014-12-03 09:23:49 +0100

    improve startup time

--- lib/czmq/utils.rb
+++ lib/czmq/utils.rb
@@ -7,8 +7,8 @@ module CZMQ
 
     ffi_lib :libzmq, :czmq
 
-    attach_function :zmq_version,   :zmq_version,     [:buffer_in, :buffer_in, :buffer_in], :void,    blocking: true
-    attach_function :zsys_version,  :zsys_version,    [:buffer_in, :buffer_in, :buffer_in], :void,    blocking: true
+    attach_function :zmq_version,   :zmq_version,     [:buffer_out, :buffer_out, :buffer_out], :void,    blocking: true
+    attach_function :zsys_version,  :zsys_version,    [:buffer_out, :buffer_out, :buffer_out], :void,    blocking: true
     attach_function :errno,         :zmq_errno,       [],                                   :int
     attach_function :strerror,      :zmq_strerror,    [:int],                               :string
 
